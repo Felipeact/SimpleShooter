a1ff3028e64c2651e0a9b2774e9dc8d9955118a5
Felipe Viana <felipetiburcioviana@gmail.com>
2023-10-29 18:41:06 -0700

    Custom BTTask

---
+++ Source/SimpleShooter/BTTask_ClearBlackBoardValue.cpp
@@ -0,0 +1,9 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BTTask_ClearBlackBoardValue.h"
+
+UBTTask_ClearBlackBoardValue::UBTTask_ClearBlackBoardValue() 
+{
+    NodeName = TEXT("Clear Black Board Value");
+}
